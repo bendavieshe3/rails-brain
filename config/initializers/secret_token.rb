3a53629709e89d5a745b324e6e2a7d69f2a7be6d
bendavies <bendavies@he3.com.au>
2013-04-04 22:30:48 +1000

    initial commit. Immediately follows rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brain::Application.config.secret_token = '107f66fe3b4c7078c6c28f29dc38247d156be5f1f718dbbf514377cf2be1bdaed929fa511cb2b782bd8a9fc320f19ea6522c26f24c2563d940d4a788aac560ae'
