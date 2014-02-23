68f9831d3b24df5321620a2585e08577f930252a
bendavies <bendavies@he3.com.au>
2014-02-23 21:28:48 +1000

    scaffold application with rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brain::Application.config.secret_key_base = 'e3f02476e47fb1fa857fa2ef829b2347612c9f7d99b3e5e55b94fcf8bdefe14763166f76e7bcfc65ccacd9cb2908a45d98cec413e917c3a2c33480ed7a4328a4'
