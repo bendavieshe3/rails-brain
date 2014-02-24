86a553c4f68f9f744f29796fa0573a8ad6efc3b5
bendavies <bendavies@he3.com.au>
2014-02-24 21:09:05 +1000

    Finish static pages for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def common_title 
+    'My Web Brain'
+  end
+
+  def title(page_title, embelish=true)
+    provide :title, if embelish 
+      "#{page_title} | #{common_title}" 
+    else
+      page_title.to_s
+    end
+  end
+
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,16 @@ describe "Static pages" do
       expect(page).to have_content('My Web Brain')
     end
 
+    it "should have the tagline 'Your Brain on the Internet'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Your Brain on the Internet')
+    end
+
+    it "should have the full name of the site in the title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("My Web Brain: Your Brain on the Internet")
+    end
+
   end
 
   describe "Help page" do
@@ -17,6 +27,12 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have it should have 'Help' as the title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help | My Web Brain")
+    end
+
   end
 
   describe "About page" do
@@ -25,6 +41,13 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About My Web Brain')
     end
+
+    it "should have it should have 'About' as the title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About | My Web Brain")
+    end
+
   end
+
 
 end
