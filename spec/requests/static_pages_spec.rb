acf7f7d7cafbe27f4d1bef50029c7b273d0c8715
bendavies <bendavies@he3.com.au>
2014-03-01 22:01:04 +1000

    Add static routes for home, about, help. improve Rspec tests for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,59 +3,31 @@ require 'spec_helper'
 describe "Static pages" do
   
   let(:common_part_of_title) { 'My Web Brain' }
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'My Web Brain'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('My Web Brain')
-    end
-
-    it "should have the tagline 'Your Brain on the Internet'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Your Brain on the Internet')
-    end
-
-    it "should have the full name of the site in the title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("My Web Brain: Your Brain on the Internet")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('Home')
-    end
-
+    it { should have_content('My Web Brain')}
+    it { should have_content('Your Brain on the Internet')}
+    it { should have_title("My Web Brain: Your Brain on the Internet")}
+    it { should_not have_title('Home')}
 
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have it should have 'Help' as the title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Help | #{common_part_of_title}")
-    end
+    it { should have_content('Help')}
+    it { have_title("Help | #{common_part_of_title}")}
 
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About My Web Brain'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About My Web Brain')
-    end
-
-    it "should have it should have 'About' as the title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("About | #{common_part_of_title}")
-    end
-
+    it { should have_content('About My Web Brain')}
+    it { should have_title("About | #{common_part_of_title}")}
   end
-
 
 end
