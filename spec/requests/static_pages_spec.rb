7ecc92a758dff1c5f2619932b9119b6574ae62ad
bendavies <bendavies@he3.com.au>
2014-03-04 23:27:59 +1000

    used a shared example in static pages tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,12 +5,19 @@ describe "Static pages" do
   let(:common_part_of_title) { 'My Web Brain' }
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_heading(heading)}
+    it { should have_title(title)}
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('My Web Brain')}
+    let(:title) { 'My Web Brain: Your Brain on the Internet' }
+    let(:heading) { 'My Web Brain' }
+
+    it_should_behave_like "all static pages"
     it { should have_content('Your Brain on the Internet')}
-    it { should have_title("My Web Brain: Your Brain on the Internet")}
     it { should_not have_title('Home')}
 
   end
@@ -18,16 +25,21 @@ describe "Static pages" do
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help')}
-    it { have_title("Help | #{common_part_of_title}")}
+    let(:title) { "Help | #{common_part_of_title}" }
+    let(:heading) { "Help" }
+
+    it_should_behave_like "all static pages"
 
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About My Web Brain')}
-    it { should have_title("About | #{common_part_of_title}")}
+    let(:title) { "About | #{common_part_of_title}" }
+    let(:heading) { "About My Web Brain" }
+
+    it_should_behave_like "all static pages"
+
   end
 
 end
